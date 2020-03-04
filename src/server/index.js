c377274afd1e101aa83fe01448359655705670f5
liran, weisbord (lw114h) <lw114h@att.com>
2020-03-04 16:38:32 +0200

    first upload

---
+++ src/server/index.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
+
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+textapi.sentiment({
+    'text': 'John is a very good football player!'
+}, function(error, response) {
+    if (error === null) {
+        console.log(response);
+    }
+});
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+app.get('/', function(req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function() {
+    console.log('Example app listening on port 8081!')
+})
+
+app.get('/test', function(req, res) {
+    res.send(mockAPIResponse)
+})
