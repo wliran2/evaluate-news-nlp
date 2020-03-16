e7d9fb87eae7a1cc9e5467ae839e513b50b7e80f
liran, weisbord (lw114h) <lw114h@att.com>
2020-03-16 18:14:36 +0200

    disabled service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -27,6 +27,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new WorkboxPlugin.GenerateSW()
+        //new WorkboxPlugin.GenerateSW()
+
     ]
 }
